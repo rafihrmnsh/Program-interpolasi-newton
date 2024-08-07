8a230b3a5c7e57c5580378472cbea95eb131871e
Muhamad Rafi Hermansah <114910465+rafihrmnsh@users.noreply.github.com>
2024-08-08 01:28:28 +0700

    Add files via upload

---
+++ tugaskelompok5.m
@@ -0,0 +1,46 @@
+clc;
+clear;
+disp('Program interpolasi newton');
+disp('==========================');
+disp(' ');
+
+n = input('Input jumlah titik = ');
+x = zeros(1, n);
+F = zeros(1, n);
+
+%Input nilai x dan F%
+for i =1:n
+  x(i) = input(['Input nilai x(', num2str(i), '): ']);
+  F(i) = input(['Input nilai F(', num2str(i), '): ']);
+end
+
+disp(' ');
+z = input('Input nilai yang akan diinterpolasi (z) = ');
+
+eps = 0;
+b = zeros(1,n);
+b(1) = F(1);
+
+tic
+pbagi = b(1);
+factor = 1;
+
+for i = 2:n
+  b(i) = F(i);
+  for j = i-1:-1:1
+    b(j) = (b(j+1) - b(j)) / (x(i) - x(j));
+  end
+
+  factor = factor * (z - x(i-1));
+  suku = b(1) * factor;
+  pbagi = pbagi + suku;
+
+  if (abs(suku) <= eps)
+    break;
+  end
+end
+
+interpolasi = pbagi;
+
+disp(' ');
+disp(['Jadi nilai taksirannya adalah ' num2str(interpolasi)]);
